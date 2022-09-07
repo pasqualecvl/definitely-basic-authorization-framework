ea7db02b5f7a838070e90d42cac537946c46efff
Pasquale Cavallo <info@pasqualecavallo.it>
2022-09-07 13:16:57 +0200

    Add authorization context example

--- src/main/java/co/develhope/project/template/controller/TestController.java
+++ src/main/java/co/develhope/project/template/controller/TestController.java
@@ -3,6 +3,7 @@ package co.develhope.project.template.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.develhope.project.template.filter.AuthenticationContext;
 import co.develhope.project.template.security.PublicEndpoint;
 import co.develhope.project.template.security.RoleSecurity;
 import co.develhope.project.template.security.ZeroSecurity;
@@ -24,13 +25,13 @@ public class TestController {
 	@ZeroSecurity
 	@GetMapping("/no-role-endpoint")
 	public void noRoleEndpoint() {
-		System.out.println("This endpoint can be reached with any role, but require authentication");
+		System.out.println("This endpoint can be reached with any role, but require authentication. Authenticated user is " + AuthenticationContext.get().getUsername());
 	}
 
 	@RoleSecurity(value = { "ROLE_ADMIN", "ROLE_PUBLISHER" })
 	@GetMapping("/role-evaluated-endpoint")
 	public void roleCheckEndpoint() {
-		System.out.println("This endpoint can be reached only by authenticated users with ROLE_ADMIN or ROLE_PUBLISHER");
+		System.out.println("This endpoint can be reached only by authenticated users with ROLE_ADMIN or ROLE_PUBLISHER. Authenticated user is " + AuthenticationContext.get().getUsername());
 	}
 
 }
